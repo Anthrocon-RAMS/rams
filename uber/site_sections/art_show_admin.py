1b081971abf02578e765364e29bbfd5f040c9ded
Victoria Earl <kitsuta@gmail.com>
2025-05-14 19:07:40 -0400

    Fix art show locations update_all bug If only one artist existed to update, this would throw an error as it tried to iterate through each character in the string.

--- uber/site_sections/art_show_admin.py
+++ uber/site_sections/art_show_admin.py
@@ -3,6 +3,7 @@ from barcode import Code39
 from barcode.writer import ImageWriter
 import re
 import math
+import six
 
 from datetime import datetime
 from decimal import Decimal
@@ -377,6 +378,9 @@ class Root:
     def update_all(self, session, message='', **params):
         if 'id' in params:
             app_list = []
+            if isinstance(params.get('id'), six.string_types):
+                params['id'] = [params.get('id')]
+
             for id in params.get('id'):
                 app_params = {key.replace(f'_{id}', ''): val for key, val in params.items() if f'_{id}' in key}
                 app_params['id'] = id
